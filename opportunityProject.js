c4a8dada38aef75153c81ac62a89aced11eaec33
philyphish <philipknigh@gmail.com>
2016-05-21 10:57:15 -0400

    Add picture to link for education

--- opportunityProject.js
+++ opportunityProject.js
@@ -9,5 +9,5 @@ $(".Col-Links a").each(function(index,value)
                        
                      })
 //myLinks is an array that contains the EIH links on the QuickSearch.jsp page
-//testing commit again!
-console.log(myLinks[1]);
+//inserts a picture into the link, below is a test picture
+$(myLinks[0]).append("<br/><a href='"+myLinks[0]+"'><img src='https://avatars3.githubusercontent.com/u/4255604?v=3&u=2b2e2dd89230fc338014de8a947af9be54727088&s=140'></a>");
