c50e86dbb62b7982e0fb1e997356722ce6eb3632
philyphish <philipknigh@gmail.com>
2016-05-21 11:02:54 -0400

    Add same pictures to each EIH link

--- opportunityProject.js
+++ opportunityProject.js
@@ -1,13 +1,10 @@
-/*Opportunity project*/
-
-//grabs the EIH intrests links and puts them into the variable myLinks
 var myLinks = [];
 $(".Col-Links a").each(function(index,value)
-                     {
-                       myLinks[index] = value;
+     {
+    	 myLinks[index] = value;
+         $(myLinks[index]).append
+         //loop through each EIH link and add a picture to each
+         //next try to add different picture to each EIH link
+         ("<br/><a href='"+myLinks[index]+"'><img src='https://avatars3.githubusercontent.com/u/4255604?v=3&u=2b2e2dd89230fc338014de8a947af9be54727088&s=140'></a>");
                        
-                       
-                     })
-//myLinks is an array that contains the EIH links on the QuickSearch.jsp page
-//inserts a picture into the link, below is a test picture
-$(myLinks[0]).append("<br/><a href='"+myLinks[0]+"'><img src='https://avatars3.githubusercontent.com/u/4255604?v=3&u=2b2e2dd89230fc338014de8a947af9be54727088&s=140'></a>");
+     })
