0b16cb6cfca869dc04a31acc09e8e38b899f5727
philyphish <philipknigh@gmail.com>
2016-05-21 11:37:35 -0400

    Add different pictures for each link

--- opportunityProject.js
+++ opportunityProject.js
@@ -1,10 +1,9 @@
 var myLinks = [];
 $(".Col-Links a").each(function(index,value)
-     {
-    	 myLinks[index] = value;
-         $(myLinks[index]).append
-         //loop through each EIH link and add a picture to each
-         //next try to add different picture to each EIH link
-         ("<br/><a href='"+myLinks[index]+"'><img src='https://avatars3.githubusercontent.com/u/4255604?v=3&u=2b2e2dd89230fc338014de8a947af9be54727088&s=140'></a>");
+                     {
+                       myLinks[index] = value;
+                       //the img src plus the index will grab the appropriate picture for the link
+                       //change the ip address to the server/directory where your pictures are stored
+                       $(myLinks[index]).append("<br/><a href='"+myLinks[index]+"'><img src='http://71.47.212.199:8080/EIH"+index+".png'></a>");
                        
-     })
+                     })
