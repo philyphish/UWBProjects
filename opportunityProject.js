5262b4e3d7ade46645bba1e2ed10fe46021d5e25
philyphish <philipknigh@gmail.com>
2016-06-02 13:21:19 -0400

    Change img link to AwesomeFont icons

--- opportunityProject.js
+++ opportunityProject.js
@@ -1,9 +1,17 @@
+$(document).ready(function(){
 var myLinks = [];
 $(".Col-Links a").each(function(index,value)
                      {
                        myLinks[index] = value;
-                       //the img src plus the index will grab the appropriate picture for the link
+                       //index is the number of teh array variable
                        //change the ip address to the server/directory where your pictures are stored
-                       $(myLinks[index]).append("<br/><a href='"+myLinks[index]+"'><img src='http://71.47.212.199:8080/EIH"+index+".png'></a>");
+                       $(myLinks[0]).html("<div class='fa fa-graduation-cap' style='font-size:40px;'><span style='font-family:verdana, geneva;'>&nbsp;Education</span></div>");
+                       $(myLinks[1]).html("<div class='fa fa-usd' style='font-size:40px;'><span style='font-family:verdana, geneva;'>&nbsp;Income</span></div>");
+                       $(myLinks[2]).html("<div class='fa fa-stethoscope' style='font-size:40px;'><span style='font-family:verdana, geneva;'>&nbsp;&nbsp;Health</span></div>");
+                       $(myLinks[3]).html("<div class='fa fa-heart-o' style='font-size:40px;'><span style='font-family:verdana, geneva;'>&nbsp;Safty Net</span></div>");
                        
                      })
+})
+
+///var/lib/tomcat7/webapps/ROOT
+//tomcat directory
